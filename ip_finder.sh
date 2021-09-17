8908ceaacbf859a913ed23a506593c3ccd372e03
Yunkai Zhang <yunkai_zhang@berkeley.edu>
2021-09-17 16:23:50 -0400

    new ipv4

--- ip_finder.sh
+++ ip_finder.sh
@@ -1,4 +1,5 @@
 touch current_ipv4.txt
 ip route get 1.2.3.4 | awk '{print $7}' 2>&1 | tee current_ipv4.txt
-curl -X DELETE https://transfer.sh/zzy_ipv4.txt
-curl --upload-file ./current_ipv4.txt https://transfer.sh/zzy_ipv4.txt
+git add .
+git commit -m 'new ipv4'
+git push -u origin main
